180b9dc635fef048a25dad9a2001393a02f1f247
Blake Kaufman <68204898+BlakeKaufman@users.noreply.github.com>
2022-08-08 12:30:34 -0400

    Add files via upload

--- buyOnline.js
+++ buyOnline.js
@@ -3,20 +3,29 @@
 // buy online animation
 const navLi = document.querySelectorAll(".buyOnlineAnimation");
 const BuyOnlineText = document.querySelectorAll(".buyOn");
+const liquid = document.querySelector(".liquid");
 let BuyTextMove = true;
 function moveText() {
   if (BuyTextMove) {
     BuyOnlineText.forEach((text) => {
       text.style.top = "5px";
       text.style.color = "white";
+      liquid.style.display = "block";
+      liquid.style.animation = "raiseLiquid 0.5s";
       text.style.transition = "all 0.5s";
     });
   } else {
     BuyOnlineText.forEach((text) => {
       text.style.top = "14px";
+      liquid.style.animation = "dropliquid 0.5s";
       text.style.color = "#e8be4c";
     });
   }
+  liquid.addEventListener("animationend", function () {
+    if (BuyTextMove) {
+      liquid.style.display = "none";
+    }
+  });
   BuyTextMove = !BuyTextMove;
 }
 navLi.forEach((nav) =>
